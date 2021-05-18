35793aaa85ced86986d3f3c280450a7f1c30a725
Sandeep Sharma <blueskymail234@gmail.com>
2021-05-18 14:31:29 +0545

    Update

--- Data manupulation in panda/subsetting.py
+++ Data manupulation in panda/subsetting.py
@@ -9,7 +9,21 @@ df[["col_a", "col_b"]]
 homelessness is available and pandas is loaded as pd.
 '''
 # Select the individuals column
-individuals = 
+individuals = homelessness['individuals']
 
 # Print the head of the result
-____
+print(individuals.head())
+
+
+# Select the state and family_members columns
+state_fam = homelessness[['state', 'family_members']]
+
+# Print the head of the result
+print(state_fam.head())
+
+
+# Select only the individuals and state columns, in that order
+ind_state = homelessness[['individuals', 'state']]
+
+# Print the head of the result
+print(ind_state.head())
