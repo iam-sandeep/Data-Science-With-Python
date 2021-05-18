37499cff3fbcb41195ebf90a21c930ee8cb40b9f
Sandeep Sharma <blueskymail234@gmail.com>
2021-05-18 14:07:20 +0545

    Update

---
+++ Data manupulation in panda/subsetting.py
@@ -0,0 +1,15 @@
+'''
+Subsetting columns
+When working with data, you may not need all of the variables in your dataset. Square brackets ([]) can be used to select only the columns that matter to you in an order that makes sense to you. To select only "col_a" of the DataFrame df, use
+
+df["col_a"]
+To select "col_a" and "col_b" of df, use
+
+df[["col_a", "col_b"]]
+homelessness is available and pandas is loaded as pd.
+'''
+# Select the individuals column
+individuals = 
+
+# Print the head of the result
+____
