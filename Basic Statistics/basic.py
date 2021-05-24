1a4a863d7d0528fcd7ee9b70247ce935c6b4458d
Sandeep Sharma <blueskymail234@gmail.com>
2021-05-24 07:31:14 +0545

    Update

--- Basic Statistics/basic.py
+++ Basic Statistics/basic.py
@@ -145,3 +145,19 @@ unemp_fuel_stats = sales.groupby("type")[["unemployment", "fuel_price_usd_per_l"
 
 # Print unemp_fuel_stats
 print(unemp_fuel_stats)
+
+'''
+Sorting by index values
+Previously, you changed the order of the rows in a DataFrame by calling .sort_values(). It's also useful to be able to sort by elements in the index. For this, you need to use .sort_index().
+
+pandas is loaded as pd. temperatures_ind has a multi-level index of country and city, and is available.
+'''
+
+# Sort temperatures_ind by index values
+print(temperatures_ind.sort_index())
+
+# Sort temperatures_ind by index values at the city level
+print(temperatures_ind.sort_index(level="city"))
+
+# Sort temperatures_ind by country then descending city
+print(temperatures_ind.sort_index(level=["country", "city"], ascending = [True, False]))
