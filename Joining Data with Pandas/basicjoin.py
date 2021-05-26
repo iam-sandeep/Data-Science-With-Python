28d67fad7ecc5dc07852c68e93176d00ea7c44c4
Sandeep Sharma <blueskymail234@gmail.com>
2021-05-26 21:45:11 +0545

    Update on Relationship

---
+++ Joining Data with Pandas/basicjoin.py
@@ -0,0 +1,13 @@
+'''
+Inner joins and number of rows returned
+All of the merges you have studied to this point are called
+ inner joins. It is necessary to understand that inner joins only return the rows 
+ with matching values in both tables.
+
+
+ONe to many Relationship
+
+with a one-to-many relationship, a row in the left table may be repeated if 
+it is related to multiple rows in the right table.
+
+'''
