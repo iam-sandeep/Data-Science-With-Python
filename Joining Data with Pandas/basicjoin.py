2d5e2b08dfa3eb624767fdd85ce195befedc8b6b
Sandeep Sharma <blueskymail234@gmail.com>
2021-05-30 15:56:01 +0545

    Update

--- Joining Data with Pandas/basicjoin.py
+++ Joining Data with Pandas/basicjoin.py
@@ -200,6 +200,73 @@ ctry_date = pd.merge_ordered(gdp,pop, on=("date","country"),
 # Print ctry_date
 print(ctry_date)
 
+# Merge gdp and pop on country and date with fill
+date_ctry = pd.merge_ordered(gdp,pop, on=("country","date"),
+                             fill_method='ffill')
+
+# Print date_ctry
+print(date_ctry)
+
+'''
+Using merge_asof() to study stocks
+You have a feed of stock market prices that you record.
+ You attempt to track the price every five minutes. Still, due to some network latency, 
+ the prices you record are roughly every 5 minutes. You pull your price logs for three
+  banks, JP Morgan (JPM), Wells Fargo (WFC), and Bank Of America (BAC).
+   You want to know how the price change of the two other banks compare
+    to JP Morgan. Therefore, you will need to merge these three logs into one table. 
+    Afterward, you will use the pandas .diff() method to compute the price change over 
+    time. Finally, plot the price changes so you can review your analysis.
+
+The three log files have been loaded for you as tables named jpm, wells, and bac.
+'''
+
+# Use merge_asof() to merge jpm and wells
+jpm_wells = pd.merge_asof(jpm, wells, on='date_time', 
+                          suffixes=('', '_wells'), direction='nearest')
+
+# Use merge_asof() to merge jpm_wells and bac
+jpm_wells_bac = pd.merge_asof(jpm_wells, bac, on='date_time', 
+                              suffixes=('_jpm', '_bac'), direction='nearest')
+
+# Compute price diff
+price_diffs = jpm_wells_bac.diff()
+
+# Plot the price diff of the close of jpm, wells and bac only
+price_diffs.plot(y=['close_jpm','close_wells','close_bac'])
+plt.show()
+
+
+
+
+'''
+Using merge_asof() to create dataset
+The merge_asof() function can be used to create datasets
+where you have a table of start and stop dates, and you want
+to use them to create a flag in another table. You have been given gdp,
+which is a table of quarterly GDP values of the US during the 1980s. Additionally, 
+the table recession has been given to you. It holds the starting date of every US recession
+since 1980, and the date when the recession was declared to be over. Use merge_asof() to
+merge the tables and create a status flag if a quarter was during a recession. Finally,
+to check your work, plot the data in a bar chart.
+
+The tables gdp and recession have been loaded for you.
+'''
+
+# Merge gdp and recession on date using merge_asof()
+gdp_recession = pd.merge_asof(gdp, recession, on="date")
+
+# Create a list based on the row value of gdp_recession['econ_status']
+is_recession = ['r' if s=='recession' else 'g' for s in gdp_recession['econ_status']]
+
+# Plot a bar chart of gdp_recession
+gdp_recession.plot(kind="bar", y="gdp", x="date", color="red", rot=90)
+plt.show()
+
+
+
+
+
 
 
 
