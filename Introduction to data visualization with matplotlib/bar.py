72369d2f33c04ed7cdce49dde3d8f835e4d193ea
Sandeep Sharma <blueskymail234@gmail.com>
2021-06-02 16:12:25 +0545

    Updated

---
+++ Introduction to data visualization with matplotlib/bar.py
@@ -0,0 +1,73 @@
+#BAr Chart
+fig, ax = plt.subplots()
+
+# Plot a bar-chart of gold medals as a function of country
+ax.bar(medals.index, medals["Gold"])
+
+# Set the x-axis tick labels to the country names
+ax.set_xticklabels(medals.index, rotation=90)
+
+# Set the y-axis label
+ax.set_ylabel("Number of medals")
+
+plt.show()
+
+
+'''
+Stacked bar chart
+A stacked bar chart contains bars, where the height of each bar represents values.
+ In addition, stacked on top of the first variable may be another variable. 
+ The additional height of this bar represents the value of this variable. 
+ And you can add more bars on top of that.
+'''
+
+# Add bars for "Gold" with the label "Gold"
+ax.bar(medals.index, medals["Gold"], label="Gold")
+
+# Stack bars for "Silver" on top with label "Silver"
+ax.bar(medals.index, medals["Silver"], bottom=medals["Gold"], label="Silver")
+
+# Stack bars for "Bronze" on top of that with label "Bronze"
+ax.bar(medals.index, medals["Bronze"], bottom=medals["Gold"] + medals["Silver"], label="Bronze")
+
+# Display the legend
+ax.legend()
+
+plt.show()
+
+
+
+#Histogram
+
+fig, ax = plt.subplots()
+# Plot a histogram of "Weight" for mens_rowing
+ax.hist(mens_rowing["Weight"])
+
+# Compare to histogram of "Weight" for mens_gymnastics
+ax.hist(mens_gymnastics["Weight"])
+
+# Set the x-axis label to "Weight (kg)"
+ax.set_xlabel("Weight (kg)")
+
+# Set the y-axis label to "# of observations"
+ax.set_ylabel("# of observations")
+
+plt.show()
+
+
+
+fig, ax = plt.subplots()
+
+# Plot a histogram of "Weight" for mens_rowing
+ax.hist(mens_rowing["Weight"], histtype='step', label="Rowing", bins=5)
+
+# Compare to histogram of "Weight" for mens_gymnastics
+ax.hist(mens_gymnastics["Weight"], histtype='step', label="Gymnastics", bins=5)
+
+ax.set_xlabel("Weight (kg)")
+ax.set_ylabel("# of observations")
+
+# Add the legend and show the Figure
+ax.legend()
+plt.show()
+
