7864d2673cc2d001e625ee04f92c817e8e025747
Sandeep Sharma <blueskymail234@gmail.com>
2021-06-06 18:34:14 +0545

    Seaborn Styles and Types

--- Intermediate Data Visualization with Seaborn/Seaborn Style/introstyle.py
+++ Intermediate Data Visualization with Seaborn/Seaborn Style/introstyle.py
@@ -12,14 +12,13 @@ plt.show()
 plt.clf()
 
 
-
 '''
 Comparing styles
 Seaborn supports setting different styles that can control the aesthetics of the final plot. 
 In this exercise,
  you will plot the same data in two different styles in order to see how the styles change the output
 '''
-# Plot with a dark style 
+# Plot with a dark style
 sns.set_style('dark')
 sns.distplot(df['fmr_2'])
 plt.show()
@@ -51,3 +50,100 @@ sns.despine()
 plt.show()
 plt.clf()
 
+
+# color style
+# Set style, enable color code, and create a magenta distplot
+sns.set(color_codes=True)
+sns.distplot(df['fmr_3'], color='m')
+
+# Show the plot
+plt.show()
+
+
+'''
+Using default palettes
+Seaborn includes several default palettes that can be easily applied to your plots. In this example, we will look at the impact of two different palettes on the same distplot.
+'''
+# Loop through differences between bright and colorblind palettes
+for p in ['bright', 'colorblind']:
+    sns.set_palette(p)
+    sns.distplot(df['fmr_3'])
+    plt.show()
+
+    # Clear the plots
+    plt.clf()
+
+    # Create the husl palette
+sns.palplot(sns.color_palette("husl", 10))
+plt.show()
+
+'''
+Using matplotlib axes
+Seaborn uses matplotlib as the underlying library for creating plots. Most of the time, you can use the Seaborn API to modify your visualizations but sometimes it is helpful to use matplotlib's functions to customize your plots. The most important object in this case is matplotlib's axes.
+
+Once you have an axes object, you can perform a lot of customization of your plot.
+
+In these examples, the US HUD data is loaded in the dataframe df and all libraries are imported.
+'''
+# Create a figure and axes
+fig, ax = plt.subplots()
+
+# Plot the distribution of data
+sns.distplot(df['fmr_3'], ax=ax)
+
+# Create a more descriptive x axis label
+ax.set(xlabel="3 Bedroom Fair Market Rent")
+
+# Show the plot
+plt.show()
+
+# ..................................................................   '''
+
+# Create a figure and axes
+fig, ax = plt.subplots()
+
+# Plot the distribution of 1 bedroom rents
+sns.distplot(df['fmr_1'], ax=ax)
+
+# Modify the properties of the plot
+ax.set(xlabel="1 Bedroom Fair Market Rent",
+       xlim=(100, 1500),
+       title="US Rent")
+
+# Display the plot
+plt.show()
+
+
+# Create a figure and axes. Then plot the data
+fig, ax = plt.subplots()
+sns.distplot(df['fmr_1'], ax=ax)
+
+# Customize the labels and limits
+ax.set(xlabel="1 Bedroom Fair Market Rent", xlim=(100, 1500), title="US Rent")
+
+# Add vertical lines for the median and mean
+ax.axvline(x=median, color='m', label='Median', linestyle='--', linewidth=2)
+ax.axvline(x=mean, color='b', label='Mean', linestyle='-', linewidth=2)
+
+# Show the legend and plot the data
+ax.legend()
+plt.show()
+
+'''
+Multiple plots
+For the final exercise we will plot a comparison of 
+the fair market rents for 1-bedroom and 2-bedroom apartments.
+'''
+# Create a plot with 1 row and 2 columns that share the y axis label
+fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, sharey=True)
+
+# Plot the distribution of 1 bedroom apartments on ax0
+sns.distplot(df['fmr_1'], ax=ax0)
+ax0.set(xlabel="1 Bedroom Fair Market Rent", xlim=(100, 1500))
+
+# Plot the distribution of 2 bedroom apartments on ax1
+sns.distplot(df['fmr_2'], ax=ax1)
+ax1.set(xlabel="2 Bedroom Fair Market Rent", xlim=(100, 1500))
+
+# Display the plot
+plt.show()
