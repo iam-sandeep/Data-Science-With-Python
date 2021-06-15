ebd7d32434c7c77a9bba93b97bbb973fb602e5f7
Sandeep Sharma <blueskymail234@gmail.com>
2021-06-15 16:56:26 +0545

    Jittering

---
+++ Explotory Data analytics in python/relationship.py
@@ -0,0 +1,65 @@
+''''
+PMF of age
+Do people tend to gain weight as they get older? We can answer this question by visualizing the relationship between weight and age. But before we make a scatter plot, it is a good idea to visualize distributions one variable at a time. Here, you'll visualize age using a bar chart first. Recall that all PMF objects have a .bar() method to make a bar chart.
+
+The BRFSS dataset includes a variable, 'AGE' (note the capitalization!), which represents each respondent's age. To protect respondents' privacy, ages are rounded off into 5-year bins. 'AGE' contains the midpoint of the bins.
+
+
+'''
+# Extract AGE
+age = brfss['AGE']
+
+# Plot the PMF
+pmf_age = Pmf(age)
+pmf_age.bar()
+
+# Label the axes
+plt.xlabel('Age in years')
+plt.ylabel('PMF')
+plt.show()
+
+'''
+Scatter plot
+Now let's make a scatterplot of weight versus age. To make the code run faster, I've selected only the first 1000 rows from the brfss DataFrame.
+
+weight and age have already been extracted for you. Your job is to use plt.plot() to make a scatter plot.
+
+'''
+# Select the first 1000 respondents
+brfss = brfss[:1000]
+
+# Extract age and weight
+age = brfss['AGE']
+weight = brfss['WTKG3']
+
+# Make a scatter plot
+plt.plot(age, weight, 'o', alpha=0.1)
+
+plt.xlabel('Age in years')
+plt.ylabel('Weight in kg')
+
+plt.show()
+
+
+
+'''
+Jittering
+In the previous exercise, the ages fall in columns because they've been rounded into 5-year bins. If we jitter them, the scatter plot will show the relationship more clearly. Recall how Allen jittered height and weight in the video:
+
+'''
+# Select the first 1000 respondents
+brfss = brfss[:1000]
+
+# Add jittering to age
+age = brfss['AGE'] + np.random.normal(0, 2.5, size=len(brfss))
+# Extract weight
+weight = brfss['WTKG3']
+
+# Make a scatter plot
+plt.plot(age, weight, 'o', markersize=5, alpha=0.2)
+
+plt.xlabel('Age in years')
+plt.ylabel('Weight in kg')
+plt.show()
+
+
