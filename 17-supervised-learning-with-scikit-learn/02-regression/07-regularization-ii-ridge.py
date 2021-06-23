e1db2e7d25920a1a43e685d7cf88dc1b4e4084a5
Sandeep Sharma <blueskymail234@gmail.com>
2021-06-23 21:49:46 +0545

    Update

--- 17-supervised-learning-with-scikit-learn/02-regression/07-regularization-ii-ridge.py
+++ 17-supervised-learning-with-scikit-learn/02-regression/07-regularization-ii-ridge.py
@@ -1,9 +1,11 @@
 '''
 Regularization II: Ridge
 
-Lasso is great for feature selection, but when building regression models, Ridge regression should be your first choice.
+Lasso is great for feature selection, but when building regression models,
+ Ridge regression should be your first choice.
 
-Recall that lasso performs regularization by adding to the loss function a penalty term of the absolute value of each coefficient multiplied by some alpha. This is also known as L1
+Recall that lasso performs regularization by adding to the loss function a
+ penalty term of the absolute value of each coefficient multiplied by some alpha. This is also known as L1
 L
 1
 regularization because the regularization term is the L1
@@ -11,10 +13,12 @@ L
 1
 norm of the coefficients. This is not the only way to regularize, however.
 
-If instead you took the sum of the squared values of the coefficients multiplied by some alpha - like in Ridge regression - you would be computing the L2
+If instead you took the sum of the squared values of the coefficients multiplied 
+by some alpha - like in Ridge regression - you would be computing the L2
 L
 2
- norm. In this exercise, you will practice fitting ridge regression models over a range of different alphas, and plot cross-validated R2
+ norm. In this exercise, you will practice fitting ridge regression models over a 
+ range of different alphas, and plot cross-validated R2
 R
 2
 scores for each, using this function that we have defined for you, which plots the R2
@@ -37,18 +41,22 @@ def display_plot(cv_scores, cv_scores_std):
     ax.set_xscale('log')
     plt.show()
 
-Don't worry about the specifics of the above function works. The motivation behind this exercise is for you to see how the R2
+Don't worry about the specifics of the above function works. The motivation
+ behind this exercise is for you to see how the R2
 R
 2
- score varies with different alphas, and to understand the importance of selecting the right value for alpha. You'll learn how to tune alpha in the next chapter.
+ score varies with different alphas, and to understand the importance of
+  selecting the right value for alpha. You'll learn how to tune alpha in the next chapter.
 
 INSTRUCTIONS
 100XP
 Instantiate a Ridge regressor and specify normalize=True.
 Inside the for loop:
 Specify the alpha value for the regressor to use.
-Perform 10-fold cross-validation on the regressor with the specified alpha. The data is available in the arrays X and y.
-Append the average and the standard deviation of the computed cross-validated scores. NumPy has been pre-imported for you as np.
+Perform 10-fold cross-validation on the regressor with the specified alpha.
+ The data is available in the arrays X and y.
+Append the average and the standard deviation of the computed cross-validated scores.
+ NumPy has been pre-imported for you as np.
 Use the display_plot() function to visualize the scores and standard deviations.
 '''
 # Import necessary modules
