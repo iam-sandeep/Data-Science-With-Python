10005c4bf12b9e6e32d74d092056e6487557a934
Sandeep Sharma <blueskymail234@gmail.com>
2021-07-04 21:44:30 +0545

    Ensemble

--- Machine Learning With Tree Based Model With Python/The Bias variance tradeoff/evaluate-the-tranning-error.py
+++ Machine Learning With Tree Based Model With Python/The Bias variance tradeoff/evaluate-the-tranning-error.py
@@ -24,3 +24,16 @@ RMSE_train = (MSE(y_train, y_pred_train))**(1/2)
 
 # Print RMSE_train
 print('Train RMSE: {:.2f}'.format(RMSE_train))
+
+
+
+
+'''
+High bias or high variance?
+In this exercise you'll diagnose whether the regression tree dt you trained in the previous exercise suffers from a bias or a variance problem.
+
+The training set RMSE (RMSE_train) and the CV RMSE (RMSE_CV) achieved by dt are available in your workspace. In addition, we have also loaded a variable called baseline_RMSE which corresponds to the root mean-squared error achieved by the regression-tree trained with the disp feature only (it is the RMSE achieved by the regression tree trained in chapter 1, lesson 3). Here baseline_RMSE serves as the baseline RMSE above which a model is considered to be underfitting and below which the model is considered 'good enough'.
+
+Does dt suffer from a high bias or a high variance problem?
+
+'''
