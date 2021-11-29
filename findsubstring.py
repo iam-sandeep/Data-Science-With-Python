afa411005f8e470400fb972ec3e764887f336a7d
Sandeep Sharma <37343905+iam-sandeep@users.noreply.github.com>
2021-11-29 23:27:26 +0545

    Update findsubstring.py

--- findsubstring.py
+++ findsubstring.py
@@ -26,3 +26,18 @@ message = "Good morning. Today is {today:%B %d, %Y}. It's {today:%H:%M} ... time
 
 # Use the format method replacing the placeholder with get_date
 print(message.format(today=get_date))
+
+
+# Import template
+from string import Template
+
+# Select variables
+our_tool = tools[0]
+our_fee = tools[1]
+our_pay = tools[2]
+
+# Create template
+course = Template("We are offering a 3-month beginner course on $tool just for $$ $fee ${pay}ly")
+
+# Substitute identifiers with three variables
+print(course.substitute(tool=our_tool, fee=our_fee, pay=our_pay))
