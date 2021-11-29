e438c56f3762beeb8a7af89df452e55edb11f49a
Sandeep Sharma <37343905+iam-sandeep@users.noreply.github.com>
2021-11-29 22:25:34 +0545

    Update findsubstring.py

--- findsubstring.py
+++ findsubstring.py
@@ -9,3 +9,20 @@ for movie in movies:
     else:
         # Replace three occurrences with one
         print(movie.replace("actor actor actor", "actor"))
+        
+        
+        
+        Replacing 
+        
+        
+        # Import datetime 
+from datetime import datetime
+
+# Assign date to get_date
+get_date = datetime.now()
+
+# Add named placeholders with format specifiers
+message = "Good morning. Today is {today:%B %d, %Y}. It's {today:%H:%M} ... time to work!"
+
+# Use the format method replacing the placeholder with get_date
+print(message.format(today=get_date))
